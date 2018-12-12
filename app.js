cfd6f6b7b53711efbd11a4ee759496a181db73ee
Neal Jay Robison <nealjrobison@gmail.com>
2018-12-12 17:12:26 -0500

    edit server in app.js

--- app.js
+++ app.js
@@ -119,6 +119,8 @@ app.use('/users', users);
  */
 var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+var server = app.listen(port, function () {
     console.log('Server running at http://127.0.0.1:' + port + '/');
 });
+
+app.set(server) = server;
