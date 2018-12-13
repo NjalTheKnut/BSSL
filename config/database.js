499e3fe4cf64cace5bc141e035391766c9e91f3e
Neal Jay Robison <nealjrobison@gmail.com>
2018-12-13 17:14:09 -0500

    change mongodb string

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database:'mongodb://localhost:27017/nodekb',
+  database:'mongodb+srv://node-app:<PASSWORD>@bssl-db-2q4nz.mongodb.net/test?retryWrites=true',
   secret: 'yoursecret'
 }
