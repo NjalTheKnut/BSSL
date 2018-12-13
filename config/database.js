2ddd79590429f5b5b60b7b2d21df31156d8b14aa
Neal Jay Robison <nealjrobison@gmail.com>
2018-12-13 17:30:21 -0500

    edit db connection string

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database:'mongodb+srv://node-app:<PASSWORD>@bssl-db-2q4nz.mongodb.net/test?retryWrites=true',
+  database:'mongodb+srv://node-app:<#1jkOhjtyO01>@bssl-db-2q4nz.mongodb.net/test?retryWrites=true',
   secret: 'yoursecret'
 }
