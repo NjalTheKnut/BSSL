45d1f7d5e1207925477c1430e6dacea71de0935e
Neal Jay Robison <nealjrobison@gmail.com>
2018-12-19 16:13:01 -0500

    change server IP listened for in app.js

--- app.js
+++ app.js
@@ -110,6 +110,6 @@ app.use('/users', users);
 // Start Server
 var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, "52.73.49.14", function () {
   console.log('Server started on port ' + port);
 });
