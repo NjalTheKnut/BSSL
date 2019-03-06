8a9e3eab67f30b487c42807ae8c8bd679f3486c7
Neal Jay Robison <nealjrobison@gmail.com>
2019-03-06 18:13:37 -0500

    refactor user edit feature

--- routes/users.js
+++ routes/users.js
@@ -88,28 +88,27 @@ router.post('/userProfile', function (req, res) {
       password: password
     });
 
-
     bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(newUser.password, salt, function (err, hash) {
+      bcrypt.hash(newUser.password, salt, function (err, hash) {
+        if (err) {
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function (err) {
           if (err) {
             console.log(err);
+            return;
+          } else {
+            req.flash('success', 'You are now registered and can log in');
+            res.redirect('/users/login');
           }
-          newUser.password = hash;
-          newUser.save(function (err) {
-            if (err) {
-              console.log(err);
-              return;
-            } else {
-              req.flash('success', 'You are now registered and can log in');
-              res.redirect('/users/login');
-            }
-          });
         });
-      }
+      });
     });
-
-  user = newUser;
+    user = newUser;
+  }
 });
+
 
 // Login Form
 router.get('/login', function (req, res) {
