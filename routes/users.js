f051bce48a427040830c840574529d6d82f5005a
Neal Jay Robison <nealjrobison@gmail.com>
2019-03-11 14:48:02 -0400

    remove redundant code

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const User = require('mongoose').model('User');
 
 // Bring in User Model
 let User = require('../models/user');
