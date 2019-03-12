619ac03be2a4496622975184c5ee92fae185e82f
Neal Jay Robison <nealjrobison@gmail.com>
2019-03-12 12:31:14 -0400

    revert slight change to render of user profile

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ app.get('/users/userProfile', function (req, res, user) {
       console.log(err);
     } else {
       res.render('userProfile', {
+        user: user,
         name: user.name,
         email: user.email,
         username: user.username,
